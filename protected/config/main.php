04a762040dc97ed487597b73a41fba5668dc0dcd
Fran <fran@ni54.com>
2016-04-25 09:10:54 -0300

    db config

--- protected/config/main.php
+++ protected/config/main.php
@@ -154,7 +154,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=bagoweb',
+			'connectionString' => 'mysql:host=localhost;dbname=fronteras',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
