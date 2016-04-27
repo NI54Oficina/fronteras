b626c69a4109f84af4832cb6154b9dc53b72ddf1
melmiranda <melania.smc@hotmail.com>
2016-04-27 15:22:44 -0300

    15:22 27-04-2016

--- protected/views/static/producir.php
+++ protected/views/static/producir.php
@@ -1,27 +1,49 @@
 <section id="producir" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	 <div id="btn-sanidad">
+ 		<p>SANIDAD</p>
+ 	 </div>
  	
  </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	<div id="btn-manejo">
+ 		<p>MANEJO</p>
+ 	</div>
  	
  </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	<div id="btn-genetica">
+ 		<p>GENÉTICA</p>
+ 	</div>
  	
  </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
- 	
+  	<div id="btn-nutricion">
+ 		<p>NUTRICION</p>
+ 	</div>	
  </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
- 	
+ 	 <div id="btn-fronteras">
+ 		<p>FRONTERAS PRODUCTIVAS</p>
+ 	</div>
  </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
- 	
+   	<div id="btn-veterinaria">
+ 		<p>RED VETERINARIA</p>
+ 	</div>	
  </div>
- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"
- </div>
+
  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
- 	
+  	<div id="btn-intereses">
+  	<p>LINKS DE INTERES</p>
+ 		
+ 	</div>
  </div>
 
 
