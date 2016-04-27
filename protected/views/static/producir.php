85b23b960d2a78d9be0c13e2d13793152b3e929f
melmiranda <melania.smc@hotmail.com>
2016-04-27 14:49:48 -0300

    14:30 27-04-2016

---
+++ protected/views/static/producir.php
@@ -0,0 +1,30 @@
+<section id="producir" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"
+ </div>
+ <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ </div>
+
+
+	
+
+</section>
