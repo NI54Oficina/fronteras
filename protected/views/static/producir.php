a085d0b2fded3558aa6104355bd431bff4b2ad19
melmiranda <melania.smc@hotmail.com>
2016-04-27 16:13:55 -0300

    16:13 27-04-2016

--- protected/views/static/producir.php
+++ protected/views/static/producir.php
@@ -1,47 +1,47 @@
 <section id="producir" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
 	 <div id="btn-sanidad">
  		<p>SANIDAD</p>
  	 </div>
  	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
  	<div id="btn-manejo">
  		<p>MANEJO</p>
  	</div>
  	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
  	<div id="btn-genetica">
  		<p>GENÉTICA</p>
  	</div>
  	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
   	<div id="btn-nutricion">
  		<p>NUTRICION</p>
  	</div>	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
  	 <div id="btn-fronteras">
- 		<p>FRONTERAS PRODUCTIVAS</p>
+ 		<p>FRONTERAS </br> PRODUCTIVAS</p>
  	</div>
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class=" gota-producir gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
    	<div id="btn-veterinaria">
- 		<p>RED VETERINARIA</p>
+ 		<p>RED</br> VETERINARIA</p>
  	</div>	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6col-xs-6">
+ <div class=" gota-producir col-lg-3 col-md-3 col-sm-3 col-xs-6">
   	<div id="btn-intereses">
-  	<p>LINKS DE INTERES</p>
+  	<p>LINKS DE </br>INTERES</p>
  		
  	</div>
  </div>
