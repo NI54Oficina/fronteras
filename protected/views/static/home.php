db61dc218cc90126372ec95e3a098adf5e2cde6a
melmiranda <melania.smc@hotmail.com>
2016-04-27 13:15:00 -0300

    13:15 27-04-2016

--- protected/views/static/home.php
+++ protected/views/static/home.php
@@ -3,8 +3,38 @@
 <?php $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"); ?>
 
 
-<body id="">
-<?php include_once("analytics.php") ?>
+<body id="home">
+<!-- <?php include_once("analytics.php") ?>
+ -->
+
+
+
+	<div class="home-upper-box col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 	
+ 	</div>
+ 	<div class="home-bottom-box col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+		<div class="hoy-box col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div></div>
+			<div></div>		
+		</div>
+
+ 		<div class="estemes-box col-lg-3 col-md-3 col-sm-3 col-xs-3">
+ 			<div></div>
+			<div></div>
+ 		</div>
+		<div class="producir-box col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div></div>
+			<div></div>
+		</div>
+		<div class="ayuda-box col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div></div>
+			<div></div>
+	    </div>
+
+	 </div> 	
+
+
 
 
 	
