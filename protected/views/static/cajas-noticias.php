af90c6470df16be28ed52c02500b7d23c02cb2e8
NI54-oficina <frantrf@gmail.com>
2016-04-27 15:35:47 -0300

    15:35 pm 27/04

---
+++ protected/views/static/cajas-noticias.php
@@ -0,0 +1,365 @@
+
+<!-- General cajas noticias-->
+<div class="box-noticias col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+	<!-- TÍTULO NOTICIAS DEL SECTOR -->
+	<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			
+				<h1>Noticias del sector</h1>
+			
+		</div>
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			
+			
+		</div>
+		
+	</div>
+	
+	<!-- Noticia 2-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Hacienda</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+				<h2>Lechería: afirman que la industria no cumple con los valores</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+	
+	<!-- Noticia 3-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Granos</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+				<h2>Fondos especulativos volvieron a apostar fuerte contra la soja</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+	
+	<!-- Noticia 4-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-2">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+				<h2>Precipitaciones pueden regresar mañana</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+
+	
+	<!-- Noticia 5-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-2">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+				<h2>Precipitaciones pueden regresar mañana</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+	
+	
+		<!-- Noticia 6-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-3">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+		</div>
+	
+	
+		<!-- Noticia 7-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-3">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>
+	
+	
+	<!-- Noticia 8-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Hacienda</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+				<h2>Lechería: afirman que la industria no cumple con los valores</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+		
+	
+	
+	<!-- Noticia 9-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Hacienda</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+				<h2>Lechería: afirman que la industria no cumple con los valores</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+	
+	
+	<!-- Noticia 10-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-2">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+				<h2>Precipitaciones pueden regresar mañana</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+
+	
+	<!-- Noticia 11-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-2">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+				<h2>Precipitaciones pueden regresar mañana</h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+	</div>	
+	
+		
+	
+		<!-- Noticia 12-->
+	<div  class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+			<!--Título de sección -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
+			</div>
+			
+			<!-- Imágen -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-img-noticias-3">
+				<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/noticias-img.png" alt="Imágen noticia" />
+			</div>
+			
+			<!-- Texto noticia -->
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
+				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
+				
+				<!-- Fecha -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<p>15-02-16 I 07:50</p>
+				</div>
+				
+			</div>
+		
+			
+		</div>
+		</div>
+	
+	
+	
+</div>	
+
+	
---
+++ protected/views/static/noticias.php
@@ -0,0 +1,5 @@
+<section class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+	<?php include_once("cajas-noticias.php") ?>
+
+</section>
