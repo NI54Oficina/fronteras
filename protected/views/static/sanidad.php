088a743a2424bcf65f1debd349153ba0c0cad0c5
melmiranda <melania.smc@hotmail.com>
2016-04-28 13:26:59 -0300

    13:26 28-04-2016

--- protected/views/static/sanidad.php
+++ protected/views/static/sanidad.php
@@ -4,31 +4,52 @@
 		
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sanidad.png" alt="Sanidad">
 		
-		</div>
+	</div>
 		
 	<div class="box-sanidad-right col-lg-10 col-md-10 col-sm-12 col-xs-12" >
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+		<div class="box-sanidad-inner">
 			<p>Enfermedades</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+	
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+		<div class="box-sanidad-inner ">
 			<p>Planes Sanitarios</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+	
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+		<div class="box-sanidad-inner">
 			<p>Vademecum</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+	
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+		<div class="box-sanidad-inner ">
 			<p>Dias de retiro de productos veterinarios</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+	
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+		<div class="box-sanidad-inner ">
 			<p>Laboratorios de Diagnostico</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+	
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+
+		<div class="box-sanidad-inner ">
 			<p>Toma de muestras</p>
 		</div>
-		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	</div>
+
+	<div class="box-sanidad-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+		<div class="box-sanidad-inner ">
 			<p>Valores constantes de animales en produccion</p>
 		</div>
-
+	</div>
 
 	</div>
 	
