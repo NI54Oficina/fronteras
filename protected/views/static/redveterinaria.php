86d4b556681150e447aa8389d9422f729c061239
melmiranda <melania.smc@hotmail.com>
2016-04-28 10:13:03 -0300

    10:15 28-04-2016

--- protected/views/static/redveterinaria.php
+++ protected/views/static/redveterinaria.php
@@ -3,7 +3,7 @@
 	<div class="map-box col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
    <!-- IFRAME DE MAPA, SETEAR LA LOCACION CORRECTA -->
-	 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.096023338846!2d-58.72043858429925!3d-34.42431595553572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca1f2712699bd%3A0x3985a0fecc05b5c6!2zQmlvZ8OpbmVzaXMgQmFnw7MgU0E!5e0!3m2!1ses!2sar!4v1461785162869" width="100%" height="100%" frameborder="1" style="border:2px solid #0078B9" allowfullscreen>
+	 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.096023338846!2d-58.72043858429925!3d-34.42431595553572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca1f2712699bd%3A0x3985a0fecc05b5c6!2zQmlvZ8OpbmVzaXMgQmFnw7MgU0E!5e0!3m2!1ses!2sar!4v1461785162869"  frameborder="1" s allowfullscreen>
 	 </iframe>
 		
 	</div>
@@ -13,6 +13,8 @@
 
 		    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/red-veterinaria.png" alt="Red Veterinaria">
 		
+			
+		  <div class="red-form">
 			<div id="provincia">
 
 				<form method="post">
@@ -48,6 +50,8 @@
 				</form>
 
 		 	 </div>
+
+		 </div>
 
 	</div>
 
