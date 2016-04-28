978b947c469477056706914e5a89699e16fb0e03
melmiranda <melania.smc@hotmail.com>
2016-04-28 12:38:32 -0300

    12:40 28-04-2016

---
+++ protected/views/static/sanidad.php
@@ -0,0 +1,38 @@
+<section id="sanidad" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+	<div class="box-sanidad-left col-lg-2 col-md-2 col-sm-12 col-xs-12"  >
+		
+			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sanidad.png" alt="Sanidad">
+		
+		</div>
+		
+	<div class="box-sanidad-right col-lg-10 col-md-10 col-sm-12 col-xs-12" >
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Enfermedades</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Planes Sanitarios</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Vademecum</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Dias de retiro de productos veterinarios</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Laboratorios de Diagnostico</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Toma de muestras</p>
+		</div>
+		<div class="box-sanidad-inner col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<p>Valores constantes de animales en produccion</p>
+		</div>
+
+
+	</div>
+	
+
+</section>
+
+
