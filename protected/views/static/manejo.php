9e4eb1bc6ddd3e67be1bbd5b6a07e0c3a8e49279
melmiranda <melania.smc@hotmail.com>
2016-04-29 14:21:40 -0300

    14:21 29-04-2016

--- protected/views/static/manejo.php
+++ protected/views/static/manejo.php
@@ -1,48 +1,54 @@
 <section id="manejo" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-	<div class="box-producir-left col-lg-2 col-md-2 col-sm-12 col-xs-12"  >
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12"  >
 		
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-manejo.svg" alt="Sanidad">
 		
 	</div>
 		
-	<div class="box-producir-right col-lg-10 col-md-10 col-sm-12 col-xs-12" >
 
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">
 		<div class="box-manejo-inner">
 			<p>Cubicacion de bañaderos</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
 		<div class="box-manejo-inner ">
 			<p>Bienestar animal</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
 		<div class="box-manejo-inner">
 			<p>Buenas practicas de uso de  productos veterinarios</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
 		<div class="box-manejo-inner ">
 			<p>Tipo de destete</p>
 		</div>
 	</div>
+
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+		<div class="box-relleno squeare" >
+			
+		</div>
+	</div>
 	
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
 		<div class="box-manejo-inner ">
 			<p>Tabla de estado corporal</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
 		<div class="box-manejo-inner ">
 			<p>Reglamentaciones agropecuarias</p>
 		</div>
-	</div>
+
 
 	</div>
 	
--- protected/views/static/nutricion.php
+++ protected/views/static/nutricion.php
@@ -6,45 +6,71 @@
 		
 	
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">
-		<div class="box-nutricion-inner">
+		<div class="box-nutricion-inner squeare">
 			<p>Tabla de Valores de Alimentos (NRC)</p>
 		</div>
 	</div>
 	
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
-		<div class="box-nutricion-inner ">
+		<div class="box-nutricion-inner squeare">
 			<p>Requerimientos nutricionales por categoria</p>
 		</div>
 	</div>
 	
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
-		<div class="box-nutricion-inner">
+		<div class="box-nutricion-inner squeare">
 			<p>Recomendaciones de confeccion de sitio</p>
 		</div>
 	</div>
 	
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
-		<div class="box-nutricion-inner ">
+		<div class="box-nutricion-inner squeare">
 			<p>Modelizador de tambo</p>
 		</div>
 	</div>
 
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
-		<div class="box-relleno ">
+		<div class="box-relleno squeare" >
 			<p>Relleno</p>
 		</div>
 	</div>
 	
 	
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12      ">	
-		<div class="box-nutricion-inner ">
+		<div class="box-nutricion-inner squeare">
 			<p>Modelizador de cria</p>
 		</div>	
 	</div>
-	
+		
+<script>
 
-	
-	
+$(document).ready(function(){
+  
+  var ancho = $('.squeare').width();
+  $('.squeare').css('min-height', ancho);
+
+});
+
+
+$(.squeare).click(function() {
+
+  $("p" ).each(function( index ) {
+
+  	// var alto_div = $('.squeare').height();
+  	// var alto_parrafo = $("p").height();
+
+   $("p").css('padding-top', '5px');
+
+  });
+});
+
+
+
+
+$( "p" ).each(function( index ) {
+  console.log( index + ": " + $( this ).text() );
+});
+</script>
 
 </section>
 
