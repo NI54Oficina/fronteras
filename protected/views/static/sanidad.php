3bb5a8b5bb2da54b830cb4980f3b7130d3d59504
melmiranda <melania.smc@hotmail.com>
2016-05-02 11:30:46 -0300

    11:30 2-05-2016

--- protected/views/static/sanidad.php
+++ protected/views/static/sanidad.php
@@ -1,38 +1,38 @@
 <section id="contenedor" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12"  >
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5"  >
 		
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-sanidad.svg" alt="Sanidad">
 		
 	</div>
 		
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">
 		<div class="box-sanidad-inner square">
 			<p>Enfermedades</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">	
 		<div class="box-sanidad-inner square">
 			<p>Planes Sanitarios</p>
 		</div>
 	</div>
 	
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">	
 		<div class="box-sanidad-inner square">
 			<p>Vademecum</p>
 		</div>
 	</div>
 	
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">
 		<div class="box-sanidad-inner square">
 			<p>Dias de retiro de productos veterinarios</p>
 		</div>
 	</div>
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 hidden-xs hidden-md hidden-sm ">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5 hidden-xs hidden-md hidden-sm ">	
 		<div class="box-relleno square " >
 			
 		</div>
@@ -40,21 +40,21 @@
 	</div>
 	
 	
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">	
 		<div class="box-sanidad-inner square">
 			<p>Laboratorios de Diagnostico</p>
 		</div>
 	</div>
 
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">	
+	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">	
 
 		<div class="box-sanidad-inner square">
 			<p>Toma de muestras</p>
 		</div>
 	</div>
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12">
+<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-12 col-xs-12 col-xl-2-5">
 		<div class="box-sanidad-inner square">
 			<p>Valores constantes de animales en produccion</p>
 		</div>
