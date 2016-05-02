4aaee3866d8ebbd6acc056d42d76e794b1e04781
melmiranda <melania.smc@hotmail.com>
2016-05-02 13:21:25 -0300

    sdfsdfsdf

--- protected/views/static/producir.php
+++ protected/views/static/producir.php
@@ -1,6 +1,6 @@
 <section id="contenedor" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 ">
 	 <div id="btn-sanidad">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/sanidad">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-sanidad.svg" alt="Sanidad">
@@ -9,7 +9,7 @@
  	
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
  	<div id="btn-manejo">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/manejo">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-manejo.svg" alt="Sanidad">
@@ -18,7 +18,7 @@
  	
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
  	<div id="btn-genetica">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/genetica">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-genetica.svg" alt="Sanidad">
@@ -27,7 +27,7 @@
  	
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
   	<div id="btn-nutricion">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/nutricion">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-nutricion.svg" alt="Sanidad">
@@ -35,7 +35,7 @@
  	</div>	
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
  	 <div id="btn-fronteras">
  	 	<a href="http://www.fronterasproductivas.com/">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-fronteras.svg" alt="Sanidad">
@@ -43,7 +43,7 @@
  	</div>
  </div>
 
- <div class=" gota-producir gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class=" gota-producir gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
    	<div id="btn-veterinaria">
    		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/redveterinaria">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-veterinarias.svg" alt="Sanidad">
@@ -51,7 +51,7 @@
  	</div>	
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-xl-2-5">
+ <div class=" gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
   	<div id="btn-intereses">
   	<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/intereses">
   		<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-links.svg" alt="Sanidad">
