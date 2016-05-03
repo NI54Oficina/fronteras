352e62a23d848fc85071df2e6e097e1a7fa14517
melmiranda <melania.smc@hotmail.com>
2016-05-03 12:04:20 -0300

    12:04 3-05-2016

--- protected/views/static/cajas-noticias.php
+++ protected/views/static/cajas-noticias.php
@@ -3,30 +3,30 @@
 <div class="box-noticias col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
 	<!-- TÍTULO NOTICIAS DEL SECTOR -->
-	<div class="col-lg-2-5" id="noticia-box">
-		
+	<div class="col-lg-2-5 col-md-2-5 col-sm-14 col-xs-12" id="noticia-box">
+		<div id="noticia-box-inner">
 		<h1 id="noticia-box-title">Noticias del sector</h1>
-		
+		</div>
 	</div>
 	
 	<!-- Noticia 2-->
-	<div  class="col-lg-2-5 col-md-2-5">
-		<div class="col-lg-12-5 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Granos</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 		
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+			<div  class=" container-texto-noticias">
 				
 				<h2>Fondos especulativos volvieron a apostar fuerte contra la soja</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -35,24 +35,26 @@
 		</div>
 	</div>	
 	
+
+
 	<!-- Noticia 3-->
-	<div  class="col-lg-2-5 col-md-2-5">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
 		
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Granos</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 		
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+			<div  class="container-texto-noticias">
 				<h2>Fondos especulativos volvieron a apostar fuerte contra la soja</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -63,24 +65,24 @@
 	</div>	
 	
 	<!-- Noticia 4-->
-	<div  class="col-lg-2-5 col-md-2-5">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
 		
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+			<div  class="container-texto-noticias-2">
 				
 				<h2>Precipitaciones pueden regresar mañana</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -92,47 +94,47 @@
 
 	
 	<!-- Noticia 5-->
-		<div class="col-lg-2-5 col-md-2-5">
-		
+		<div class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		  <div>
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+			<div  class="container-texto-noticias-2">
 				<h2>Precipitaciones pueden regresar mañana</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
 			</div>
-		
+		 </div>
 			
 		</div>
 
 	
 	
 		<!-- Noticia 6-->
-	<div  class="col-lg-2-5 col-md-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+			<div  class="container-texto-noticias-3">
 				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -144,22 +146,22 @@
 	
 	
 		<!-- Noticia 7-->
-	<div  class="col-lg-2-5 col-md-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+			<div  class="container-texto-noticias-3">
 				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -171,22 +173,22 @@
 	
 	
 	<!-- Noticia 8-->
-	<div  class="col-lg-2-5 col-md-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Hacienda</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+			<div  class="container-texto-noticias">
 				<h2>Lechería: afirman que la industria no cumple con los valores</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -199,22 +201,22 @@
 	
 	
 	<!-- Noticia 9-->
-	<div  class="col-lg-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Hacienda</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias">
+			<div  class=" container-texto-noticias">
 				<h2>Lechería: afirman que la industria no cumple con los valores</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -226,22 +228,22 @@
 	
 	
 	<!-- Noticia 10-->
-	<div  class="col-lg-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+			<div  class="container-texto-noticias-2">
 				<h2>Precipitaciones pueden regresar mañana</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
@@ -253,50 +255,48 @@
 
 	
 	<!-- Noticia 11-->
-	<div  class="col-lg-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Clima</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-2">
+			<div  class="container-texto-noticias-2">
 				<h2>Precipitaciones pueden regresar mañana</h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
-			</div>
-		
-			
+			</div>	
 		</div>
 	</div>	
 	
 		
 	
 		<!-- Noticia 12-->
-	<div  class="col-lg-2-5">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div  class="col-lg-2-5 col-md-2-5 col-sm-4 col-xs-12">
+		<div class="">
 		
 			<!--Título de sección -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div  class="">
 				<div class="border-noticias-h1 b-r"></div><h1>Mercado</h1><div class="border-noticias-h1 b-l"></div>
 			</div>
 
 			
 			<!-- Texto noticia -->
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-texto-noticias-3">
+			<div  class="container-texto-noticias-3">
 				<h2>Por la suba en el precio de la carne, aumenta el consumo de pollo </h2>
 				<p>Los administradores de fondos especulativos que operan en el mercado de Chicago comenzaron nuevamente a realizar apuestas bajistas en soja al tiempo que...</p>
 				
 				<!-- Fecha -->
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
 					<p>15-02-16 I 07:50</p>
 				</div>
 				
