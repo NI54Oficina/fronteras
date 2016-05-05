b577fb461bb1a0241be215ccb387f2ba928882d9
NI54-oficina <frantrf@gmail.com>
2016-05-05 18:40:38 -0300

    19:02 pm

--- protected/views/static/header.php
+++ protected/views/static/header.php
@@ -3,36 +3,31 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-logos">
 
 	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-		<img style="    width: 150px;
+		<img style="    width: 112px;
     float: left;
     padding-left: 50px;
-    padding-top: 25px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" />
+    padding-top: 10px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" />
 	</div>
 	
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 		<img style="    float: right;
     padding-right: 50px;
-    padding-top: 35px;
-    width: 250px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-bago.png" />
+    padding-top: 13px;
+    width: 100px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/100x100.png" />
 	</div>
 
 </div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top: -1px;">
-		<img style="width:100vw;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/flecos-header.png" />
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 flecos-header" style="margin-top: -1px;">
+		<!--<img style="width:100vw;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/flecos-header.png" />-->
+		<div></div>
 </div>
 
 <div "col-lg-12 col-md-12 col-sm-12 col-xs-12" style=" padding-top: 60px;">
 <span  class=" glyphicon glyphicon-menu-hamburger navbar-toggle collapsed" data-toggle="collapse" data-target="#inner-header" aria-expanded="false" aria-controls="navbar" ></span>	
-	<div id="inner-header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse navbar-collapse" data-toggle="collapse" style="width: 100vw;
-    text-align: left;
-    background: white;
-    padding: 10px 0px;
-    z-index: -1;
-    margin-top: -25px;
-    padding-top: 35px;">
-		<ul class="ul-nav col-lg-12 col-md-12 col-sm-11 col-xs-9" id="ul-nav-principal" style="">
-	<li class="li-header "><p  class="toggle-dropdown-header" target="#drop1" ></p>
+	<div id="inner-header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse navbar-collapse" data-toggle="collapse" style="width: 100vw;text-align: left;background: #F7F7F7; padding: 0px 0px;z-index: -1; margin-top: -31px; padding-top: 35px;">
+		<ul class="ul-nav col-lg-12 col-md-12 col-sm-11 col-xs-9" id="ul-nav-principal">
+		<li class="li-header "><p  class="toggle-dropdown-header" target="#drop1" ></p>
 		
 						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/hoy">Hoy</a></li>
 						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/link2">Este mes</a></li>
@@ -43,8 +38,8 @@
 
 		</ul>
 		
-	</li>
-	</ul>
+		</li>
+		</ul>
 	</div>
 </div>
 </header>
