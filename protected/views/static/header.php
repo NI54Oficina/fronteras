5fdaf21585ae9b5b12fb3856d7fc5536c88b8755
NI54-oficina <frantrf@gmail.com>
2016-05-06 19:09:02 -0300

    19:08

--- protected/views/static/header.php
+++ protected/views/static/header.php
@@ -1,53 +1,44 @@
 <header class="headerDesktop" >
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-logos">
-
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-<<<<<<< HEAD
-		<img style="    width: 112px;
-    float: left;
-    padding-left: 50px;
-    padding-top: 10px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" />
-=======
-		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/home">
-		<img style="    width: 150px;
-    float: left;
-    padding-left: 50px;
-    padding-top: 25px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" />
-    </a>
->>>>>>> origin/master
-	</div>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-logos">
 	
+		<!--Logo fronteras -->
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-		<img style="    float: right;
-    padding-right: 50px;
-    padding-top: 13px;
-    width: 100px;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/100x100.png" />
+		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/home">
+			<img class="logo-fronteras" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" />
+
+		</a>
+
+		</div>
+	
+		<!-- Logo bagó -->
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+			<img class="logo-bago-header"  src="<?php echo Yii::app()->request->baseUrl; ?>/img/100x100.png" />
+		</div>
+
 	</div>
 
-</div>
-
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 flecos-header" style="margin-top: -1px;">
-		<!--<img style="width:100vw;" src="<?php echo Yii::app()->request->baseUrl; ?>/img/flecos-header.png" />-->
+	<!--Flecos-->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 flecos-header" style="margin-top: -1px;">
 		<div></div>
-</div>
+	</div>
 
 <div "col-lg-12 col-md-12 col-sm-12 col-xs-12" style=" padding-top: 60px;">
 <span  class=" glyphicon glyphicon-menu-hamburger navbar-toggle collapsed" data-toggle="collapse" data-target="#inner-header" aria-expanded="false" aria-controls="navbar" ></span>	
-	<div id="inner-header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse navbar-collapse" data-toggle="collapse" style="width: 100vw;text-align: left;background: #F7F7F7; padding: 0px 0px;z-index: -1; margin-top: -31px; padding-top: 35px;">
+	<div id="inner-header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse navbar-collapse header-palabras-menu" data-toggle="collapse">
 		<ul class="ul-nav col-lg-12 col-md-12 col-sm-11 col-xs-9" id="ul-nav-principal">
-		<li class="li-header "><p  class="toggle-dropdown-header" target="#drop1" ></p>
+			<li class="li-header "><p  class="toggle-dropdown-header" target="#drop1" ></p>
 		
-						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/hoy">Hoy</a></li>
-						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/link2">Este mes</a></li>
-						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/producir">Producir +</a></li>
-						<li><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/link2">Ayuda</a></li>
+						<li id="hoy-header-m" class=" col-lg-1 col-md-1 col-sm-6 col-xs-6"><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/hoy">Hoy</a></li>
+						<li  id="mes-header-m" class=" col-lg-1 col-md-1  col-sm-6 col-xs-6"><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/link2">Este mes</a></li>
+						<li  id="producir-header-m" class=" col-lg-1 col-md-1  col-sm-6 col-xs-6"><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/producir">Producir +</a></li>
+						<li  id="ayuda-header-m" class="  col-lg-1 col-md-1  col-sm-6 col-xs-6"><a class="a-sub link-menu col-lg-12 col-md-12 col-sm-12 col-xs-12"	href="<?php echo Yii::app()->paisChecker->getBaseUrl(true); ?>/link2">Ayuda</a></li>
 		
-		<ul id="drop1" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dropdown-header-nav">
+				<ul id="drop1" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dropdown-header-nav">
 
-		</ul>
+				</ul>
 		
-		</li>
+			</li>
 		</ul>
 	</div>
 </div>
