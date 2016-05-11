8737089e29c25353b097115da00e56263435a074
NI54-oficina <frantrf@gmail.com>
2016-05-11 18:45:36 -0300

    .

--- protected/views/static/noticia.php
+++ protected/views/static/noticia.php
@@ -6,9 +6,21 @@ $Criteria = new CDbCriteria();
 
 ?>
 
-<h1><?php echo $nota->titulo; ?></h1>
-<p><?php echo $nota->fecha; ?></p>
-<p><?php echo $nota->categoria; ?></p>
-<h3><?php echo $nota->bajada; ?></h3>
-<p><?php echo $nota->body; ?></p>
-<p><?php echo $nota->fuente; ?></p>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-inside-noticia">
+
+	<div class="border-fecha">
+		<div></div>
+		<p 	class="fecha"><?php echo $nota->fecha; ?></p>
+	</div>
+	
+	<h1  class="titulo"><?php echo $nota->titulo; ?></h1>
+	
+	<p 	class="categoria"><?php echo $nota->categoria; ?></p>
+	
+	<h3	class="bajada"><?php echo $nota->bajada; ?></h3>
+	
+	<p	class="cuerpo"><?php echo $nota->body; ?></p>
+	
+	<p	class="fuente"><?php echo $nota->fuente; ?></p>
+	
+</div>
