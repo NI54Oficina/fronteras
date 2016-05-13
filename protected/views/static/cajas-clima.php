bfdfe700b72b7631ea4b2fac9413fa5464a28aff
Fran <fran@ni54.com>
2016-05-13 14:43:42 -0300

    13-05 14:43

--- protected/views/static/cajas-clima.php
+++ protected/views/static/cajas-clima.php
@@ -33,9 +33,6 @@
 		foreach($f as $f2){
 	 ?>
 
-	<div  class="col-lg-3 col-md-4 col-sm-6 col-xs-12 square box-clima-sp" >
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-clima-inner" hid="1">
-
 	<div  class="col-lg-3 col-md-4 col-sm-6 col-xs-12 " >
 
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box-clima-inner" hid="1"  >
