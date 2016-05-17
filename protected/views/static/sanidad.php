bb718b28db8c55c8af21136a226be109c36c3eef
NI54-oficina <frantrf@gmail.com>
2016-05-17 18:30:48 -0300

    .

--- protected/views/static/sanidad.php
+++ protected/views/static/sanidad.php
@@ -7,21 +7,26 @@
 	</div>
 		
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
-		<div class="box-sanidad border-shadow">
-			<div class="box-sanidad-inner square">
-				<p class="long-word">Enfermedades</p>
-			</div>
-		</div>	
-	</div>
+	<a href="">
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p class="long-word">Enfermedades</p>
+				</div>
+			</div>	
+		</div>
+	</a>
 	
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
-		<div class="box-sanidad border-shadow">
-			<div class="box-sanidad-inner square">
-				<p>Planes Sanitarios</p>
+	<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/planes-sanitarios">
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p>Planes Sanitarios</p>
+				</div>
 			</div>
 		</div>
-	</div>
+	</a>
+	
 	
 	<a href="http://www.biogenesisbago.com/ar/vademecum/id1/" >
 	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
@@ -33,57 +38,68 @@
 	</div>
 	</a>
 
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
-		<div class="box-sanidad border-shadow">
-			<div class="box-sanidad-inner square">
-				<p class="long-word">Días de retiro de productos veterinarios</p>
-			</div>
-
-		</div>
-	</div>
-
-
 	
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
-		<div class="box-sanidad border-shadow">
-			<div class="box-sanidad-inner square">
-				<p class="long-word">Laboratorios de Diagnóstico</p>
+	<a href="">
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p class="long-word">Días de retiro de productos veterinarios</p>
+				</div>
+	
+			</div>
+		</div>
+	</a>
+
+
+	<a href="">	
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p class="long-word">Laboratorios de Diagnóstico</p>
+				</div>
+			</div>	
+		</div>
+	</a>
+
+	<a href="">
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p>Toma de muestras</p>
+				</div>
+			</div>
+		</div>
+	</a>
+
+	<a href="">
+		<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
+			<div class="box-sanidad border-shadow">
+				<div class="box-sanidad-inner square">
+					<p class="long-word">Valores constantes de animales en producción</p>
+				</div>
 			</div>
 		</div>	
-	</div>
+	</a>
 
-
-	<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">	
-    	<div class="box-sanidad border-shadow">
-			<div class="box-sanidad-inner square">
-				<p>Toma de muestras</p>
+	<a href="">	
+		<div class="box-producir-2 box-relleno col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7 hidden-xs hidden-sm hidden-md">
+			<div class="box-sanidad">
+				<div class="box-sanidad-inner square">
+					<p></p>
+				</div>
 			</div>
 		</div>
-	</div>
+	</a>
 
-<div class="box-producir-2 col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7">
-	<div class="box-sanidad border-shadow">
-		<div class="box-sanidad-inner square">
-			<p class="long-word">Valores constantes de animales en producción</p>
-		</div>
-	</div>
-</div>
-
-	<div class="box-producir-2 box-relleno col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7 hidden-xs hidden-sm hidden-md">
-		<div class="box-sanidad">
-			<div class="box-sanidad-inner square">
-				<p></p>
+	<a href="">
+		<div class="box-producir-2 box-relleno col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7 hidden-xs hidden-sm hidden-md">
+			<div class="box-sanidad">
+				<div class="box-sanidad-inner  square">
+					<p></p>
+				</div>
 			</div>
 		</div>
-	</div>
-
-	<div class="box-producir-2 box-relleno col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7 hidden-xs hidden-sm hidden-md">
-		<div class="box-sanidad">
-			<div class="box-sanidad-inner  square">
-				<p></p>
-			</div>
-		</div>
-	</div>
+	</a>
 
 
 	<div class="box-producir-2 box-relleno col-lg-2-5 col-md-2-5 col-sm-3 col-xs-6 col-xl-7 hidden-xs hidden-sm hidden-md hidden-lg-2">
