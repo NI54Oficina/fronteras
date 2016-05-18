bde2107364ba42c6bdae8ba67c81338041c57609
NI54-oficina <frantrf@gmail.com>
2016-05-18 15:03:30 -0300

    .

--- protected/views/static/estemes.php
+++ protected/views/static/estemes.php
@@ -1,5 +1,5 @@
 <section class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section">
 	
-	 <h1>  - ESTE MES -</h1>
-	 <h2> Página en contrucción </h2>
+	<?php include_once("cajas-estemes.php") ?>
+	
 </section>
