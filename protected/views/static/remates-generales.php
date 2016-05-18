a7b542ee726b4ef2808e27c8e44d76f9b6293c99
NI54-oficina <frantrf@gmail.com>
2016-05-18 16:39:24 -0300

    16:40

---
+++ protected/views/static/remates-generales.php
@@ -0,0 +1,30 @@
+
+<!-- REMATES GENERALES -->
+
+<section class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section" id="remates-generales">
+
+	<!-- Caja general -->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"c>
+	
+		<!-- Caja general fecha -->
+		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+		
+			<!-- Titulo FECHA -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"> Fecha</div>
+		
+			<!-- Caja fecha particular -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 square"></div>
+		
+		</div>
+		
+		
+		<!-- Caja general: modalidad, consignatorio, lugar, provincia, cabezas -->
+		<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+		
+		
+		
+		</div>
+	
+	</div>
+
+</section>
