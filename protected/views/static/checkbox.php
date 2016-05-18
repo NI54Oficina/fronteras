133a5c08450b0097ebd6a6f2c7809ad7b80de1fb
NI54-oficina <frantrf@gmail.com>
2016-05-18 15:41:17 -0300

    .

---
+++ protected/views/static/checkbox.php
@@ -0,0 +1,34 @@
+<section class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section" id="checkbox">
+	
+	<h1>Checkbox</h1>
+	
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	
+		<!-- Caja  principal nombre -->
+		<div class="col-lg-2-6 checkbox-box square" id="nombre-checkbox">
+			<h2 class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Nombre <br /> de producción</h2>
+		</div>
+	
+		<div class="col-lg-2-6 checkbox-box square">
+			<img class="col-lg-12 col-md-12 col-sm-12 col-xs-12" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes-01.svg" />
+		</div>
+		
+		<div class="col-lg-2-6 checkbox-box square">
+		
+		</div>
+		
+		<div class="col-lg-2-6 checkbox-box square">
+		
+		</div>
+		
+		<div class="col-lg-2-6 checkbox-box square">
+		
+		</div>
+		
+		<div class="col-lg-2-6 checkbox-box square">
+		
+		</div>
+		
+	</div>
+	
+</section>
