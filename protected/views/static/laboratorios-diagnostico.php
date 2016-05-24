4974c2612b77377693229e2d744a463166f82503
melmiranda <melania.smc@hotmail.com>
2016-05-24 12:25:24 -0300

    12:25 24-05-2016

--- protected/views/static/laboratorios-diagnostico.php
+++ protected/views/static/laboratorios-diagnostico.php
@@ -1,13 +1,14 @@
 <section id="" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section">
 	
 	
-		<div class="box-producir-3 col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xl-12" >
-			<div class="box-sanidad-2">
-				<div class="box-sanidad-inner">
-					<p class="long-word" id="planes-h1">Laboratorios de diagnóstico</p>
-				</div>
-			</div>
-		</div>	
+<div class="box-producir-3 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div class="box-sanidad border-shadow">
+		<div class="box-sanidad-inner ">
+			<p class="center-to-parent color-sanidad planes-h1">Laboratorios de diagnóstico</p>
+		</div>
+	</div>
+</div>
+
 	
 
 	
