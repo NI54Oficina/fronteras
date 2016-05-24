0255042a66911a803aae45f972730fc12942806a
NI54-oficina <frantrf@gmail.com>
2016-05-24 16:08:31 -0300

    16:08 pm 24/05

--- protected/views/static/header.php
+++ protected/views/static/header.php
@@ -27,15 +27,23 @@
 <span  class=" glyphicon glyphicon-menu-hamburger navbar-toggle collapsed" data-toggle="collapse" data-target="#inner-header" aria-expanded="false" aria-controls="navbar" ></span>	
 	<div id="inner-header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse navbar-collapse header-palabras-menu" data-toggle="collapse">
 		<ul class="ul-nav col-lg-12 col-md-12 col-sm-11 col-xs-9" id="ul-nav-principal">
-			<li class="li-header "><p  class="toggle-dropdown-header" target="#drop1" ></p>
-		
-						<li id="hoy-header-m" class=" col-lg-1 col-md-1 col-sm-3 col-xs-6">
+			<p  class="toggle-dropdown-header" target="#drop1" ></p>
+			
+			<div class="home-upper-box col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xl-12">
+				<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12"> 
+					<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" alt="Logo Fronteras">
+					<img class="hidden-xs" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-bago.png" alt="Logo Bago " />
+				</div>
+			</div>
+			
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cont-menu-mob">
+						<li id="hoy-header-m" class=" col-lg-1 col-md-1 col-sm-3 col-xs-3">
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/hoy" class="link-menu">
 							
-								<div class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 .col-xl-4">
+								<div class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-12 col-xl-4">
 							
 									<div class="hoy-box square">
-										<div  style="height: 160px; width: 50vw;">
+										<div  class="cont-section-mobile">
 											<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-hoy.svg" alt="Hoy" />
 										</div>
 										<p class="p-header-mobile">Hoy</p>		
@@ -45,12 +53,12 @@
 
 						</li>
 
-						<li  id="mes-header-m" class=" col-lg-1 col-md-1  col-sm-3 col-xs-6">	
+						<li  id="mes-header-m" class=" col-lg-1 col-md-1  col-sm-3 col-xs-5">	
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/estemes"  class="link-menu">
 								
-								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">	
+								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-5 col-xl-4">	
 									<div class="estemes-box square ">
-										<div style="height: 160px; width: 50vw;">
+										<div  class="cont-section-mobile">
 											<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes.svg" alt="Este Mes" />
 										</div>
 										<p class="p-header-mobile">Este mes</p>
@@ -64,7 +72,7 @@
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/producir" class="link-menu">
 								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">	
 									<div class="producir-box square">
-										<div style="height: 160px; width: 50vw;">
+										<div  class="cont-section-mobile">
 											<!-- <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-producir.png" alt="Chat" /> -->
 										</div>
 										<p class="p-header-mobile">Producir +</p>
@@ -77,7 +85,7 @@
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ayuda" class="link-menu">
 								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">	
 									<div class="ayuda-box square">
-										<div style="height: 160px; width: 50vw;">
+										<div class="cont-section-mobile">
 											<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-chat.svg" alt="Chat" />
 										</div>
 										<p class="p-header-mobile">Ayuda</p>
@@ -86,12 +94,11 @@
 								</div>
 							</a>
 						</li>
-		
+					</div>
 				<ul id="drop1" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 dropdown-header-nav">
 
 				</ul>
 		
-			</li>
 		</ul>
 	</div>
 </div>
