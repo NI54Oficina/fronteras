d05b27022eefe51ef48a81dde9f7627a90a394a4
melmiranda <melania.smc@hotmail.com>
2016-05-30 10:20:53 -0300

    10:20 30-05-2016

--- protected/views/static/checkbox.php
+++ protected/views/static/checkbox.php
@@ -11,7 +11,7 @@
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	
 		<!-- Caja  principal nombre -->
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container">
 
 		<div id="nombre-checkbox" class="square" >
 			<h2 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 center-to-parent">Nombre <br /> de producción
@@ -20,35 +20,35 @@
 
 		</div>
 	
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container">
 			<div class="checkbox-box square">
 				
 			</div>
 
 		</div>
 		
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container">
 			<div class="checkbox-box square">
 				
 			</div>
 
 		</div>
 		
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container ">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container ">
 			<div class="checkbox-box square">
 				
 			</div>
 
 		</div>
 		
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container">
 			<div class="checkbox-box square">
 				
 			</div>
 
 		</div>
 		
-		<div class="col-lg-2 col-sm-4 col-md-6 col-xs-6 checkbox-container ">
+		<div class="col-lg-2 col-sm-3 col-md-3 col-xs-6 checkbox-container ">
 			<div class="checkbox-box square">
 				
 			</div>
