3ca706ec9782905ba348558415952b5e571277ba
melmiranda <melania.smc@hotmail.com>
2016-05-30 10:16:56 -0300

    10:15 30-05-2016

--- protected/views/static/intereses.php
+++ protected/views/static/intereses.php
@@ -2,7 +2,7 @@
 
 <div class="col-xs-12 hidden-lg hidden-sm hidden-md hidden-xl titulo-mobile-producir titulo-mobile"> <p> Producir + > Links de intereses </p> </div>
 
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7"  >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7"  >
 		<div class="box-interes-img square">
 			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-links.svg" alt="Red Links de Intereses">
 		</div>
@@ -10,7 +10,7 @@
 	</div>
 
 	<a href="http://www.cra.org.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 "   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 "   >
 		<p hid="1"> CRA</p>
 		<div class="box-interes-inner square cra-link">
 			
@@ -21,7 +21,7 @@
 	</a>
 
 	<a href="http://www.crea.org.ar/" target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7"   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7"   >
 		<p hid="1"> Crea</p>
 		<div class="box-interes-inner square crea-link">
 		</div>
@@ -32,7 +32,7 @@
 
 
 	<a href="http://www.sra.org.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 "   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 "   >
 		<p hid="1"> Sociedad Rural Argentina</p>
 		<div class="box-interes-inner square sra-link">
 			
@@ -44,7 +44,7 @@
 
 
 	<a href="http://inta.gob.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 "   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 "   >
 		<p hid="1"> INTA </p>
 		<div class="box-interes-inner square inta-link">
 			
@@ -55,7 +55,7 @@
 	</a>
 
 	<a href="http://www.senasa.gov.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 "   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 "   >
 		<p hid="1"> SENASA </p>
 		<div class="box-interes-inner square senasa-link">
 			
@@ -67,7 +67,7 @@
 
 
 	<a href="http://www.feedlot.com.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 "   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 "   >
 		<p hid="1"> Cámara Argentina de Feedlot </p>
 		<div class="box-interes-inner square feedlot-link">
 			
@@ -79,9 +79,9 @@
 
 
 	<a href="http://www.ipcva.com.ar/"  target="_blank">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7"   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7"   >
 		<p hid="1"> IPCVA</p>
-		<div class="box-interes-inner square ipvca-link">
+		<div class="box-interes-inner square ipcva-link">
 			
 			
 		</div>
@@ -91,7 +91,7 @@
 
 
 	<a href="#">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 box-relleno hidden-xs hidden-sm hidden-md"   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 box-relleno hidden-xs hidden-sm hidden-md"   >
 		<p hid="1"> Asociación</p>
 		<div class="box-interes-inner-relleno square">
 			
@@ -102,7 +102,7 @@
 	</a>
 
 	<a href="#">
-	<div class="box-interes col-lg-2-5 col-md-4 col-sm-4 col-xs-6 col-xl-7 box-relleno hidden-xs hidden-sm hidden-md"   >
+	<div class="box-interes col-lg-2-5 col-md-3 col-sm-3 col-xs-6 col-xl-7 box-relleno hidden-xs hidden-sm hidden-md"   >
 		<p hid="1"> Asociación</p>
 		<div class="box-interes-inner-relleno square">
 			
