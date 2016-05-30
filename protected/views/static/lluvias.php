f3f0b04e2c4f633abd39383ebaa265bd98287318
melmiranda <melania.smc@hotmail.com>
2016-05-30 13:50:06 -0300

    13:49 30-05-2016

---
+++ protected/views/static/lluvias.php
@@ -0,0 +1,99 @@
+<section class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section">
+<!-- Div contenedor -->
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-lluvias">
+<!-- Div contenedor de primer fecha -->
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 n-lluvia ">
+
+<!-- titulo primer fecha -->
+<h1 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-lluvia"> Martes 54 de Mayo </h1>
+
+<!-- tres mapas -->
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 map-lluvia ">
+
+	<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-clima" >
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  border-mapa">
+			<h3>Mapa de temperaturas mínimas</h3>
+		</div>
+	
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12    box-clima-mapas-inner" hid="3"  >
+
+			<!-- Clima datos & clima imágen -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+				<!--Clima imágen-->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-img" >
+					<img src='http://news.agrofy.com.ar/sites/default/files/tmax_844.jpg' >
+				
+					
+				</div>
+				
+			</div>
+
+			
+		</div>
+	</div>	
+	
+</div>
+
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  map-lluvia ">
+
+	<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-clima" >
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  border-mapa">
+			<h3>Mapa de temperaturas mínimas</h3>
+		</div>
+	
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12    box-clima-mapas-inner" hid="3"  >
+
+			<!-- Clima datos & clima imágen -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+				<!--Clima imágen-->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-img" >
+					<img src='http://news.agrofy.com.ar/sites/default/files/tmax_844.jpg' >
+					
+				</div>
+				
+			</div>
+
+			
+		</div>
+	</div>
+	
+</div>
+
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4  map-lluvia ">
+
+<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-clima" >
+		
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12  border-mapa">
+			<h3>Mapa de temperaturas mínimas</h3>
+		</div>
+	
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12    box-clima-mapas-inner" hid="3"  >
+
+			<!-- Clima datos & clima imágen -->
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+				<!--Clima imágen-->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mapa-img" >
+					<img src='http://news.agrofy.com.ar/sites/default/files/tmax_844.jpg'>
+					
+				</div>
+				
+			</div>
+
+			
+		</div>
+	</div>
+	
+</div>
+
+
+</div>
+
+</div>
+</section>
