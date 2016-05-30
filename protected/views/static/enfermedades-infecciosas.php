13206d886c10d31cc5a767c5dfb9311b0d142f47
melmiranda <melania.smc@hotmail.com>
2016-05-30 11:53:06 -0300

    11:52 30-05-2016

--- protected/views/static/enfermedades-infecciosas.php
+++ protected/views/static/enfermedades-infecciosas.php
@@ -15,6 +15,49 @@ $nota = Notas::model()->findByAttributes(array("id"=>1,"seccion"=>"enfermedad"))
 </div>	
 
 
+<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-enfermedades">
+
+	<div class="col-lg-2 col-md-3 col-sm-3 col-xs-12 selector-side" > 
+		<form method="post">
+         	<select name="enfermedades-select" class="enfermedades-select">
+				<option value="" selected disabled>Enfermedades</option>
+				<option value="">Enfermedad 1</option>
+				<option value="">Enfermedad 2</option>	
+				<option value="">Enfermedad 3</option>	
+				<option value="">Enfermedad 4</option>	
+			</select>
+					 
+		</form>
+		
+		<div class="enfermedades-inside-title square">
+			<h2 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 center-to-parent">
+				<?php echo $nota->titulo; ?>
+			</h2>
+		</div>
+	</div>
+
+	<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12"> 
+	
+
+	<!-- Contenido de la enfermedad -->
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 enfermedades-inside-container">
+	
+	<?php echo $nota->texto; ?>
+	
+	</div>
+	</div>
+
+</div>
+
+
+
+ <!-- TODA LA INFO COMENTADA -->
+ 
+ 
+<?php if(false){ ?>
+
+
+
 	<!-- Título -->
 	<h2 class="enfermedades-inside-title col-lg-12 col-md-12 col-sm-12 col-xs-12"><?php echo $nota->titulo; ?></h2>
 	
@@ -29,5 +72,8 @@ $nota = Notas::model()->findByAttributes(array("id"=>1,"seccion"=>"enfermedad"))
 	
 	<!-- WIDGET-->
 	<?php include_once("widget-enfermedades.php") ?>
+	
+<?php } ?>	
+	
 	
 </section>
