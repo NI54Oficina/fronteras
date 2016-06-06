2cb92cdeb2aefa640ddb229409d0056190c42292
Melania Miranda <melania.smc@hotmail.com>
2016-06-06 10:44:59 -0300

    10:44 6-6-2016

--- protected/views/static/splash-home.php
+++ protected/views/static/splash-home.php
@@ -3,14 +3,12 @@
 		<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" alt="Logo Fronteras">
 	</div>
 
-	<div class=" col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 " style="margin-top: 10%;">
+	<div class=" col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 " style="margin-top: 25%;">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="bg-square-2"></div>
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="bg-square-2"></div>
 	</div>
 
-	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 slogan-splash">
-		<p>SLOGAN FRONTERAS</p>
-	</div>
+
 	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 img-2-splash">
  		<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-bago.png" alt="Logo Bago " />
 	</div>
