5418c628c2a325c3bf3e56766c22116e7445a737
Melania Miranda <melania.smc@hotmail.com>
2016-06-06 13:37:56 -0300

    13:40 6-6-2016

--- protected/views/static/header.php
+++ protected/views/static/header.php
@@ -23,21 +23,21 @@ $("body").on("touchstart",".navbar-toggle",function(){
 				$("section").show();
 				$(document).scrollTop(scrollTop);
 				setTimeout(function(){ $(".fadder").css("opacity",1); $("section").css("opacity",1); },500);
-				
+
 				//$(".fadder").fadeIn(1000);
 			}
-			
+
 		},500);
 	}
 });
 $("body").on("mousedown",".navbar-toggle",function(){
-	
+
 });
 </script>
 <header class="headerDesktop" >
 
 	<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs header-logos">
-	
+
 		<!--Logo fronteras -->
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ar/home">
@@ -46,7 +46,7 @@ $("body").on("mousedown",".navbar-toggle",function(){
 		</a>
 
 		</div>
-	
+
 		<!-- Logo bagó -->
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 			<img class="logo-bago-header"  src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-BB-horizontal.png" />
@@ -59,7 +59,7 @@ $("body").on("mousedown",".navbar-toggle",function(){
 <div  class="">
 	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 col-xs boton-volver-header hidden-lg hidden-md hidden-sm"  style="height:100%;">
 		<button class="boton-header"  id="back-header" style="display:inline-block;overflow:hidden;" type="button"></button>
-	</div> 
+	</div>
 <span  class=" glyphicon glyphicon-th-large navbar-toggle collapsed" data-toggle="collapse" data-target="#inner-header" aria-expanded="false" aria-controls="navbar" ></span>
 
 
@@ -69,17 +69,17 @@ $("body").on("mousedown",".navbar-toggle",function(){
 
 
 
-	
+
 		<ul class="ul-nav col-lg-12 col-md-12 col-sm-11 col-xs-9" id="ul-nav-principal">
-			
-			
+
+
 			<div class="home-upper-box col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xl-12">
-				<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12"> 
+				<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-fronteras.png" alt="Logo Fronteras">
 					<img class="hidden-xs" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo-bago.png" alt="Logo Bago " />
 				</div>
 			</div>
-			
+
 
 	<!-- BOTON VOLVER -->
 	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 hidden-xs boton-volver-header" style="height:100%;">
@@ -89,49 +89,49 @@ $("body").on("mousedown",".navbar-toggle",function(){
 	<!-- TERMINA BOTON VOLVER -->
 
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cont-menu-mob center-to-parent">
-	
+
 
 
 
 
 						<li id="hoy-header-m" class=" col-lg-1 col-md-1 col-sm-3 col-xs-3">
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/hoy" class="link-menu">
-							
+
 								<div class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-12 col-xl-4">
-							
+
 									<div class="hoy-box square" onlymobile="true">
 										<div  class="cont-section-mobile">
 											<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-hoy.svg" alt="Hoy" />
 										</div>
-										<p class="p-header-mobile">Hoy</p>		
+										<p class="p-header-mobile">Hoy</p>
 									</div>
-								</div>	
+								</div>
 							</a>
 
 						</li>
-						
+
 						<div class="square-separador"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/cuadrados-separadores.svg" alt="cuadrado-separador" /></div>
 
-						<li  id="mes-header-m" class=" col-lg-1 col-md-1  col-sm-3 col-xs-5">	
+						<li  id="mes-header-m" class=" col-lg-1 col-md-1  col-sm-3 col-xs-5">
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/estemes"  class="link-menu">
-								
-								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-5 col-xl-4">	
+
+								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-5 col-xl-4">
 									<div class="estemes-box square " onlymobile="true">
 										<div  class="cont-section-mobile">
 											<img  src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes.svg" alt="Este Mes" />
 										</div>
 										<p class="p-header-mobile">Este mes</p>
 									</div>
-								
+
 								</div>
-							</a>							
+							</a>
 						</li>
-						
+
 						<div class="square-separador"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/cuadrados-separadores.svg" alt="cuadrado-separador" /></div>
-						
+
 						<li  id="producir-header-m" class=" col-lg-1 col-md-1  col-sm-3 col-xs-6">
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/producir" class="link-menu">
-								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">	
+								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">
 									<div class="producir-box square" onlymobile="true">
 										<div  class="cont-section-mobile">
 											<!-- <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-producir.png" alt="Chat" /> -->
@@ -141,25 +141,25 @@ $("body").on("mousedown",".navbar-toggle",function(){
 								</div>
 							</a>
 						</li>
-						
+
 						<div class="square-separador"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/cuadrados-separadores.svg" alt="cuadrado-separador" /></div>
-						
+
 						<li  id="ayuda-header-m" class="  col-lg-1 col-md-1  col-sm-3 col-xs-6">
 							<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ayuda" class="link-menu">
-								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">	
+								<div  class="home-box col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xl-4">
 									<div class="ayuda-box square" onlymobile="true">
 										<div class="cont-section-mobile" onlymobile="true">
 											<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-chat.svg" alt="Chat" />
 										</div>
 										<p class="p-header-mobile">Ayuda</p>
 									</div>
-								
+
 								</div>
 							</a>
 						</li>
 					</div>
-				
-		
+
+
 		</ul>
 	</div>
 </div>
