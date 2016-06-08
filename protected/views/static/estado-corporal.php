d7cdbf4a68d19874f46f91868e40b7752ad9bec8
Melania Miranda <melania.smc@hotmail.com>
2016-06-08 15:34:43 -0300

    15:35 8-6-2016

--- protected/views/static/estado-corporal.php
+++ protected/views/static/estado-corporal.php
@@ -3,99 +3,99 @@
 <div class="box-producir-3 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	<div class="box-manejo border-shadow">
 		<div class="box-manejo-inner ">
-			<p class="center-to-parent color-manejo planes-h1">Tabla estado Corporal</p>
+			<p class="center-to-parent color-manejo planes-h1">Tabla estado corporal</p>
 		</div>
 	</div>
 </div>
 
 <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12" align="center">
-	
+
 		<!-- Enfermedades infecciosas  -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
-		
+
 		<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
-	
-					<p class="p-estado-1">Condición corporal para la raza</p>
+
+					<p class="p-estado-1">Condición corporal </br> para la raza</p>
 					<p class="p-estado-2">ANGUS</p>
 				</div>
-				
+
 			</div>
-			
-		</div>	
+
+		</div>
 		</a>
-	
-	
+
+
 		<!-- Parásitos externos -->
 		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado"   class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
-	
-					<p class="p-estado-1">Condición corporal para la raza</p>
+
+					<p class="p-estado-1">Condición corporal </br> para la raza</p>
 					<p class="p-estado-2">HEREFORD</p>
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
 		</a>
-	
-		
-		
+
+
+
 		<!-- Parásitos internos -->
 		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
-	
-					<p class="p-estado-1">Condición corporal para la raza</p>
+
+					<p class="p-estado-1">Condición corporal</br> para la raza</p>
 					<p class="p-estado-2">BRANGUS</p>
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
 		</a>
-	
-		
-		
+
+
+
 		<!--  -->
 		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
-	
-					<p class="p-estado-1">Condición corporal para la raza</p>
+
+					<p class="p-estado-1">Condición corporal</br> para la raza</p>
 					<p class="p-estado-2">BRAFORD</p>
 
 				</div>
-				
-			</div>	
-			
+
+			</div>
+
 		</div>
 		</a>
-	
-		
-		
+
+
+
 		<!--  -->
 		<a href="#"  class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
-	
-						
+
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
 		</a>
-	
-		
+
+
 	</div>
 
 </section>
