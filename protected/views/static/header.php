0710b1c88459c214bf8ac71e0d3988bbf7e1888d
Melania Miranda <melania.smc@hotmail.com>
2016-06-08 13:02:25 -0300

    13:05 8-7-2016

--- protected/views/static/header.php
+++ protected/views/static/header.php
@@ -88,16 +88,16 @@ $("body").on("mousedown",".navbar-toggle",function(){
 			</div>
 
 
-	<!-- BOTON VOLVER -->
-	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 hidden-xs boton-volver-header" style="height:100%;">
-		<button class="boton-header center-to-parent-t"  id="back-header" style="" type="button"></button><p class="back-p center-to-parent-m" style="" >Volver</p>
-	</div>
 
-	<!-- TERMINA BOTON VOLVER -->
 
 					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cont-menu-mob center-to-parent">
 
+						<!-- BOTON VOLVER -->
+						<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 hidden-xs boton-volver-header" style="height:100%;">
+							<button class="boton-header center-to-parent col-lg-1 col-md-1 col-sm-1 col-xs-1"  id="back-header" style="" type="button"></button><p class="back-p col-lg-1 col-md-1 col-sm-1 col-xs-1 center-to-parent" style="" >Volver</p>
+						</div>
 
+						<!-- TERMINA BOTON VOLVER -->
 
 
 
