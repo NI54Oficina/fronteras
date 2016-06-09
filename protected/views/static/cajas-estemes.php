83929023c7398610bb1d7b31b0bcb54d92c2b751
Melania Miranda <melania.smc@hotmail.com>
2016-06-09 10:10:57 -0300

    10:10 9-06-2016

--- protected/views/static/cajas-estemes.php
+++ protected/views/static/cajas-estemes.php
@@ -7,16 +7,16 @@
 
 	<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/remates-generales" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
 	<div class="maxboxes col-lg-12 col-md-12 col-sm-12 col-xs-12 square" hid="1">
-		<div class="maxbox-remgenerales maxboxes-inner-estemes square  col-lg-12 col-md-12 col-sm-12 col-xs-12" hid="6">
+		<div class="maxbox-remgenerales maxboxes-inner-estemes square  col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector center-to-parent " hid="5">
+			<div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector " >
 				<!-- Imágen-->
 				<img class=" estemes-img"  src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes-02.svg"  />
 				<br />
 
 
 			<!--Título-->
-			<h2 hid="4" >Remates generales</h2>
+			<h2  >Remates generales</h2>
 
 			</div>
 		</div>
@@ -29,16 +29,16 @@
 	<!-- Remates cabañas -->
 	 <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/remates-cabanias" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
 	<div class="maxboxes col-lg-12 col-md-12 col-sm-12 col-xs-12 square" hid="1">
-		<div class="maxbox-remcabania maxboxes-inner-estemes square col-lg-12 col-md-12 col-sm-12 col-xs-12" hid="6">
+		<div class="maxbox-remcabania maxboxes-inner-estemes square col-lg-12 col-md-12 col-sm-12 col-xs-12" >
 
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector center-to-parent " hid="5">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector ">
 				<!-- Imágen-->
 				<img class=" estemes-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes-01.svg"  />
 				<br />
 
 
 			<!--Título-->
-			<h2 hid="4" >Remates cabañas</h2>
+			<h2  >Remates cabañas</h2>
 
 			</div>
 		</div>
@@ -53,16 +53,16 @@
 <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/checkbox" class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
 	<div class="maxboxes col-lg-12 col-md-12 col-sm-12 col-xs-12 square" hid="1">
 
-		<div class="maxbox-checkbox maxboxes-inner-estemes square col-lg-12 col-md-12 col-sm-12 col-xs-12 " hid="6">
+		<div class="maxbox-checkbox maxboxes-inner-estemes square col-lg-12 col-md-12 col-sm-12 col-xs-12 " >
 
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector center-to-parent" hid="5">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 container-noticias-sector " >
 				<!-- Imágen-->
 				<img class=" estemes-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/icono-este-mes-03.svg"  />
 				<br />
 
 
 			<!--Título-->
-			<h2 hid="4" >Checkbox</h2>
+			<h2 >Checkbox</h2>
 				</div>
 
 		</div>
