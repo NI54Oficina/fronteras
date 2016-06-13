e8eaa36f14e931565b7e23d0a942dc1f76e987e0
Melania Miranda <melania.smc@hotmail.com>
2016-06-13 13:08:50 -0300

    13:10 13-06-2016

--- protected/views/static/producir.php
+++ protected/views/static/producir.php
@@ -2,42 +2,42 @@
 
 <div class="col-xs-12 hidden-lg hidden-sm hidden-md hidden-xl titulo-mobile-producir titulo-mobile"> <p> Producir + </p> </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 ">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3 ">
 	 <div id="btn-sanidad">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/sanidad">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-sanidad.svg" alt="Sanidad">
  		</a>
  	 </div>
- 	
+
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
  	<div id="btn-manejo">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/manejo">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-manejo.svg" alt="Manejo">
  		</a>
  	</div>
- 	
+
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
  	<div id="btn-genetica">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/genetica">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-genetica.svg" alt="Genetica">
  		</a>
  	</div>
- 	
+
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
   	<div id="btn-nutricion">
  		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/nutricion">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-nutricion.svg" alt="Nutricion">
  		</a>
- 	</div>	
+ 	</div>
  </div>
 
- <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class="gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
  	 <div id="btn-fronteras">
  	 	<a href="http://www.fronterasproductivas.com/">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-fronteras.svg" alt="">
@@ -45,15 +45,15 @@
  	</div>
  </div>
 
- <div class=" gota-producir gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class=" gota-producir gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
    	<div id="btn-veterinaria">
    		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/redveterinaria">
  			<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-veterinarias.svg" alt="">
- 		</a>	
- 	</div>	
+ 		</a>
+ 	</div>
  </div>
 
- <div class=" gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6">
+ <div class=" gota-producir col-lg-3 col-md-3 col-sm-6 col-xs-6 col-lan-xs-3">
   	<div id="btn-intereses">
   	<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/intereses">
   		<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gota-links.svg" alt="">
@@ -62,6 +62,6 @@
  </div>
 
 
-	
+
 
 </section>
