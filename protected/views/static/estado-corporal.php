e1a60f19789e5ea1a9419594cafa8689f78dee2e
Melania Miranda <melania.smc@hotmail.com>
2016-06-23 12:22:09 -0300

    12:22 23-06-2016

--- protected/views/static/estado-corporal.php
+++ protected/views/static/estado-corporal.php
@@ -14,7 +14,7 @@
 <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12" align="center">
 
 		<!-- Enfermedades infecciosas  -->
-		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
+		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado/angus"  class="">
 
 		<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
@@ -31,7 +31,7 @@
 
 
 		<!-- Parásitos externos -->
-		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado"   class="">
+		<a href="<?php echo Yii::app()->getBaseUrl(true); ?>/ganado/hereford"   class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
@@ -49,7 +49,7 @@
 
 
 		<!-- Parásitos internos -->
-		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
+		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado/brangus"  class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
@@ -67,7 +67,7 @@
 
 
 		<!--  -->
-		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado"  class="">
+		<a href="?php echo Yii::app()->getBaseUrl(true); ?>/ganado/braford"  class="">
 			<div class="col-lg-2-5 col-sm-4 col-md-2-5 col-xs-6 col-xl-5 col-lan-xs-4 max-box-planes">
 			<div class=" col-lg-12 col-sm-12 col-md-12 col-xs-12 square planes box-manejo">
 				<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 center-to-parent p-estado">
