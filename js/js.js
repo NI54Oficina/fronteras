7721203803c89f1c82937be1a62970cbe6691279
Melania Miranda <melania.smc@hotmail.com>
2016-06-27 10:56:41 -0300

    .

--- js/js.js
+++ js/js.js
@@ -398,19 +398,6 @@ function CenterToParent(){
 			$(this).css("padding-top",paddingTop+"px");
 		}
 	});
-	$(".center-to-parent-d").each(function(){
-		$(this).css("margin-top",0);
-		$(this).css("margin-bottom",0);
-		$(this).css("padding-bottom",0);
-		$(this).css("padding-top",0);
-		var parent= $(this).parent();
-		console.log($(this).outerHeight());
-		if($(this).outerHeight()>0){
-			var paddingTop= ($(parent).innerHeight()/2)-($(this).outerHeight()/2);
-			paddingTop= paddingTop/2;
-			$(this).css("padding-top",paddingTop+"px");
-		}
-	});
 	$(".center-to-parent-m").each(function(){
 		$(this).css("margin-top",0);
 		$(this).css("margin-bottom",0);
