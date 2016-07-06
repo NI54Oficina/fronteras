6be27cdceed1bba7e35cf71b90bfcd8a668d756b
Fran <fran@ni54.com>
2016-07-06 10:18:28 -0300

    06-07 10:18

--- protected/views/static/loading.php
+++ protected/views/static/loading.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xl-12 loader">
 
 		<div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xl-12 loading-gif ">
-			 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/loading2.gif">
+			 <img style="max-width:80%;" src="<?php echo Yii::app()->request->baseUrl; ?>/images/loading2.gif">
 		</div>
 
 </div>
