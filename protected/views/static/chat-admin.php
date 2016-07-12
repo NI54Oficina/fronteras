99e50f2a4e3cb792cdeda97659d9c827f5049912
Melania Miranda <melania.smc@hotmail.com>
2016-07-12 12:43:38 -0300

    12:43 12-07-2016

--- protected/views/static/chat-admin.php
+++ protected/views/static/chat-admin.php
@@ -16,7 +16,7 @@ $chats= Chat::model()->findAll($Criteria);
 	foreach($chats as $chat){
 	?>
 		<div class="chatSelector" idChat="<?php echo $chat->id; ?>"><h3><?php echo $chat->nombre; ?></h3>
-		<p><?php echo $chat->motivo; ?></p>
+		<p class="motive"><?php echo $chat->motivo; ?></p>
 		<a class="details-admin" href="<?php echo Yii::app()->getBaseUrl(true)."/ar/chat/view/".$chat->id; ?>" target="_blank">Ver detalles</a>
 		<button type="button" class="closeChat">Finalizar chat</button>
 		</div>
