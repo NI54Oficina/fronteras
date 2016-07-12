e4138fe8d618cbf30ceba5125538fc59a3595137
Melania Miranda <melania.smc@hotmail.com>
2016-07-12 12:34:39 -0300

    12.34 12-07-2016

--- protected/views/static/chat-admin.php
+++ protected/views/static/chat-admin.php
@@ -1,5 +1,11 @@
-<div class="col-lg-4 adminLateral" style="background-color:red;color:white;">Modo Admin
-<a href="<?php echo Yii::app()->getBaseUrl(true)."/ar/chat/admin/"; ?>">Historial</a>
+<div class="col-lg-4 adminLateral" >
+	<div class="info-ayuda-admin">
+		<h1>AYUDA</h1>
+		<p>
+			Modo Admin | <a href="<?php echo Yii::app()->getBaseUrl(true)."/ar/chat/admin/"; ?>">Historial</a>
+		</div>
+		</p>
+
 <?php
 
 $Criteria = new CDbCriteria();
@@ -11,7 +17,7 @@ $chats= Chat::model()->findAll($Criteria);
 	?>
 		<div class="chatSelector" idChat="<?php echo $chat->id; ?>"><h3><?php echo $chat->nombre; ?></h3>
 		<p><?php echo $chat->motivo; ?></p>
-		<a href="<?php echo Yii::app()->getBaseUrl(true)."/ar/chat/view/".$chat->id; ?>" target="_blank">Ver detalles</a>
+		<a class="details-admin" href="<?php echo Yii::app()->getBaseUrl(true)."/ar/chat/view/".$chat->id; ?>" target="_blank">Ver detalles</a>
 		<button type="button" class="closeChat">Finalizar chat</button>
 		</div>
 	<?php } ?>
