86bf8405c4c0f1979abe27c5893b2d26ccfd81bd
Fran <fran@ni54.com>
2016-07-13 10:56:05 -0300

    13-07 10:55

--- js/jsApp.js
+++ js/jsApp.js
@@ -43,4 +43,52 @@ ExitFunction= function(){
 }
 $("body").on("click","a",function(e){
 	e.preventDefault();
+	
 });
+
+
+GoUrlLink= function(toGo,popArray){
+	if (toGo == null) {
+		return;
+	}
+	if(toGo!="home"){
+		$(".glyphicon").show();
+		$(".boton-volver-header").show();
+	}
+	
+	 if(toGo.indexOf(baseURL)>=0){
+		if(popArray&&lastUrl!=""&&lastUrl!=toGo){
+			urlHistory.push(lastUrl);
+		}
+		lastUrl=toGo;
+		toGo= toGo.replace(baseURL,"");
+		
+		$("#containerApp").html("");
+		loadPage(toGo);
+		//$("header .glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-th-large");
+		//$("header").css("position","fixed");
+		
+		if(popArray){
+		hasChanged=true;
+		window.location.hash = "aux"+auxInt++;
+		}
+	 }else if(toGo.indexOf("http")>=0){
+		//window.open(toGo);
+		cordova.InAppBrowser.open(toGo, '_blank', 'location=yes');
+	 }else{
+		if(popArray&&lastUrl!=""&&lastUrl!=toGo){
+			urlHistory.push(lastUrl);
+		}
+		lastUrl=toGo;
+		//$("header .glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-th-large");
+		//$("header").css("position","fixed");
+		
+		$("#containerApp").html("");
+		loadPage(toGo);
+		if(popArray){
+			hasChanged=true;
+			window.location.hash = "aux"+auxInt++;
+		}		
+	 }
+}
+$("body").append("<style>#inner-header{padding-top:15px;}.boton-volver-header{padding-top:15px;}.navbar-toggle2{padding-top:23px;}.titulo-mobile{padding-top:15px;}</style>");
