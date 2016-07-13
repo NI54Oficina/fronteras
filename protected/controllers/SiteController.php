ab29293aabe04f9e255399140232f0d389d0e192
Melania Miranda <melania.smc@hotmail.com>
2016-07-13 11:43:15 -0300

    11:43

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -254,9 +254,9 @@ class SiteController extends Controller
 					}
 
 					if($admin){
-				 	fwrite(fopen($file, 'a'), "<p class='admin-nick'>". $nickname . "</p> <p class='admin-message'> " . $message = str_replace("\n", " ", $message)."</p>" . "\n");
+				 	fwrite(fopen($file, 'a'), "<div class='admin-message-container'><p class='admin-nick'>". $nickname . "</p> <p class='admin-message'> " . $message = str_replace("\n", " ", $message)."</p></div><br>" . "\n");
 				 }else{
-					 fwrite(fopen($file, 'a'), "<p class='user-nick'>". $nickname . "</p> <p class='user-message'> " . $message = str_replace("\n", " ", $message)."</p>" . "\n");
+					 fwrite(fopen($file, 'a'), "<div class='user-message-container'><p class='user-nick'>". $nickname . "</p> <p class='user-message'> " . $message = str_replace("\n", " ", $message)."</p></div><br>" . "\n");
 				 }
 			 }
 				 break;
